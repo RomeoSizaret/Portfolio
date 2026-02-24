81ede6a1cb9f01cabbcc3571456b971d16ecb74b
RomeoSizaret <sizaretromeo@gmail.com>
2026-02-24 17:21:00 +0100

    update

--- scripts.js
+++ scripts.js
@@ -74,3 +74,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+document.addEventListener("scroll", function () {
+  const nav = document.querySelector(".side-navigation");
+
+  const scrollPosition = window.innerHeight + window.scrollY;
+  const pageHeight = document.body.offsetHeight;
+
+  if (scrollPosition >= pageHeight - 50) {
+    nav.classList.add("visible");
+  } else {
+    nav.classList.remove("visible");
+  }
+});
